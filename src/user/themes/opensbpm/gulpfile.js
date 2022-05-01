ec16247e5a7d51aea0f91477c4d30fba649d47aa
Stefan Sedelmaier <stefan@sedelmaier.at>
2022-05-01 16:32:49 +0200

    add gulp-sourcemaps

--- src/user/themes/opensbpm/gulpfile.js
+++ src/user/themes/opensbpm/gulpfile.js
@@ -2,11 +2,14 @@
 
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const sourcemaps = require('gulp-sourcemaps');
 
 function buildStyles() {
-  return gulp.src('./sass/**/*.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./css-compiled'));
-};
+    return gulp.src('./sass/**/*.scss')
+            .pipe(sourcemaps.init())
+            .pipe(sass().on('error', sass.logError))
+            .pipe(sourcemaps.write('.'))
+            .pipe(gulp.dest('./css-compiled'));
+}
 
 exports.buildStyles = buildStyles;
